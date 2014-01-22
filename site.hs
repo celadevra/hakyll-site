aa54ad3a0841ff5f754104e43f3d5d83a397bf82
Haoyang Xu <home@idenizen.net>
2014-01-22 14:58:03 +0800

    Add tags functionality and presentation to site
    The codes are basically copied from Gwern's site.

--- site.hs
+++ site.hs
@@ -7,6 +7,8 @@ import           Control.Monad (forM, liftM)
 --import           System.Time (formatCalendarTime, toUTCTime)
 import           Data.List (sortBy)
 import           Data.Ord (comparing)
+import           Text.Pandoc (HTMLMathMethod(MathJax),
+                    ObfuscationMethod(ReferenceObfuscation), WriterOptions(..))
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -23,17 +25,20 @@ main = hakyll $ do
        route    idRoute
        compile  copyFileCompiler
 
+    -- Tags
+    tags <- buildTags "**.page" (fromCapture "tags/*")
+
     match "docs/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+        compile $ pandocCompilerWith defaultHakyllReaderOptions woptions
+            >>= loadAndApplyTemplate "templates/default.html" (postCtx tags)
             >>= relativizeUrls
 
-    match "*.page" $ do
+    match "**.page" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
+        compile $ pandocCompilerWith defaultHakyllReaderOptions woptions
             >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= loadAndApplyTemplate "templates/default.html" (postCtx tags)
             >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
@@ -45,14 +50,18 @@ main = hakyll $ do
         compile $ do
             posts <- fmap (take 10) . createdFirst =<<
                 loadAllSnapshots "*.page" "content"
-            renderRss feedConfiguration feedContext posts
+            renderRss feedConfiguration (feedContext tags) posts
 
-
+    tagsRules tags $ \tag pattern -> do
+                 let title = "Tag: " ++ tag
+                 route idRoute
+                 compile $ tagPage tags title pattern
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
+postCtx :: Tags -> Context String
+postCtx tags =
     dateField "created" "%Y-%m-%d" `mappend`
     modificationTimeField "updated" "%Y-%m-%d" `mappend`
+    tagsField "tags" tags `mappend`
     defaultContext
 
 feedConfiguration :: FeedConfiguration
@@ -64,8 +73,8 @@ feedConfiguration = FeedConfiguration
     , feedRoot        = "http://idenizen.net"
     }
 
-feedContext :: Context String
-feedContext = postCtx `mappend`
+feedContext :: Tags -> Context String
+feedContext tags = (postCtx tags) `mappend`
     modificationTimeField "published" "%Y-%m-%d" `mappend`
     mconcat
     [ rssBodyField "description"
@@ -87,9 +96,35 @@ rssBodyField :: String -> Context String
 rssBodyField key = field key $
     return . itemBody
 
+woptions :: WriterOptions
+woptions = defaultHakyllWriterOptions{ writerSectionDivs = True,
+                                       writerStandalone = True,
+                                       writerTableOfContents = True,
+                                       writerColumns = 70,
+                                       writerTemplate = "<div id=\"TOC\">$toc$</div>\n$body$",
+                                       writerHtml5 = True,
+                                       writerHTMLMathMethod = Text.Pandoc.MathJax "",
+                                       writerEmailObfuscation = ReferenceObfuscation}
+
 createdFirst :: [Item String] -> Compiler [Item String]
 createdFirst items =
     let itemsWithTime = unsafeCompiler $ forM items $ \item -> do
         utc <- getModificationTime $ toFilePath $ itemIdentifier item
         return (utc,item);
     in liftM (\xs -> reverse . map snd $ sortBy (comparing fst) xs) itemsWithTime
+
+postList :: Tags -> Pattern -> ([Item String] -> Compiler [Item String]) -> Compiler String
+postList tags pattern preprocess' = do
+    postItemTemplate <- loadBody "templates/postitem.html"
+    posts' <- loadAll pattern
+    posts <- preprocess' posts'
+    applyTemplateList postItemTemplate (postCtx tags) posts
+
+tagPage :: Tags -> String -> Pattern -> Compiler (Item String)
+tagPage tags title pattern = do
+    list <- postList tags pattern (return . id)
+    makeItem ""
+        >>= loadAndApplyTemplate "templates/tags.html"
+                (constField "posts" list `mappend` constField "title" title `mappend`
+                    defaultContext)
+        >>= relativizeUrls
