e3b99b216ba82131604ba858249b181b888340cc
Haoyang Xu <home@idenizen.net>
2014-02-10 17:33:39 +0800

    Change domain name and S3 bucket name

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./site build
-s3cmd sync _site/ s3://haoyangwrit.es --guess-mime-type --mime-type 'text/html' --recursive --delete-removed --acl-public
+s3cmd sync _site/ s3://expoundite.net --guess-mime-type --mime-type 'text/html' --recursive --delete-removed --acl-public
