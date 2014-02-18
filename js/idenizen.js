92b997523128b871ff44c9da3413421e709f9a4e
Haoyang Xu <home@idenizen.net>
2014-02-18 12:39:42 +0800

    Add mail link; optimize index page display

--- js/idenizen.js
+++ js/idenizen.js
@@ -1,9 +1,9 @@
 $('document').ready(function() {
-    $('div.tapestry').hide();
+    $('span.tapestry').hide();
     $('span.button > a').mouseover(function() {
         $(this).parent().children('.tapestry').show().fadeIn(200);
     });
     $('span.button > a').mouseout(function() {
-        $('div.tapestry').hide();
+        $('span.tapestry').hide();
     });
 });
