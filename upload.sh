0b8686a2dfd7564e91b7a2ca787e245a2c53fb31
Haoyang Xu <github@expoundite.net>
2014-05-05 15:43:48 +0800

    optimize upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git checkout master # only this branch gets published, allowing drafts
+git stash save -u "Clean state for publishing" # move things under carpet
 ./site build
 s3cmd sync _site/ s3://expoundite.net --guess-mime-type --mime-type 'text/html' --recursive --delete-removed --acl-public
+git stash pop # release dirties from under the carpet
