e574e0ccb45318fd1783e3eb6d5e6b6d4e71cdf4
Haoyang Xu <github@expoundite.net>
2014-05-05 15:28:59 +0800

    optimize upload script

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+git checkout master # only this branch gets published, allowing drafts
 ./site build
 s3cmd sync _site/ s3://expoundite.net --guess-mime-type --mime-type 'text/html' --recursive --delete-removed --acl-public
