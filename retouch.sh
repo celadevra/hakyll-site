5b762a9b18f037b579e6f1839c8b51f8e7560b30
Celad Evra <celadevra@users.noreply.github.com>
2014-06-22 07:10:11 +0800

    Update retouch.sh

--- retouch.sh
+++ retouch.sh
@@ -3,5 +3,5 @@
 # give the last commit time to generated html as modification time
 for i in `find . -name "*page" -type f | sed -e 's/^.\///'`; do
   MTIME=`git log -1 --pretty=format:%ai $i | awk '{print $1$2}' | sed -e 's/[-]//g' -e 's/[:]//' -e 's/:/\./'`;
-  touch -m -t $MTIME ./_site/`dirname $i`/`basename $i .page`
+  touch -m -t $MTIME ./$i
 done
