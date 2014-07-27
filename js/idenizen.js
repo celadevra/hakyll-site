b8d4df0560ea9295639c243010febf2e9a317bfd
Haoyang Xu <github@expoundite.net>
2014-07-27 21:13:15 +0800

    adjust file struct

--- js/idenizen.js
+++
@@ -1,9 +0,0 @@
-$('document').ready(function() {
-    $('span.tapestry').hide();
-    $('span.button > a').mouseover(function() {
-        $(this).parent().children('.tapestry').show().fadeIn(200);
-    });
-    $('span.button > a').mouseout(function() {
-        $('span.tapestry').hide();
-    });
-});
