08ed9e045d07dbff44619e311a44fa9912b9cf4a
Haoyang Xu <github@expoundite.net>
2014-08-07 02:56:59 +0800

    change search path

--- retouch.sh
+++ retouch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # give the last commit time to generated html as modification time
-for i in `find . -name "*page" -type f | sed -e 's/^.\///'`; do
+for i in `find ./expoundite.net/ -name "*page" -type f | sed -e 's/^.\///'`; do
   MTIME=`git log -1 --pretty=format:%ai $i | awk '{print $1$2}' | sed -e 's/[-]//g' -e 's/[:]//' -e 's/:/\./'`;
   touch -m -t $MTIME ./$i
 done
