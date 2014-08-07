e00d00249c4e692232390a37761f21432f583d43
Haoyang-docker <github@expoundite.net>
2014-08-07 06:55:39 +0000

    enable rss for main articles

--- expoundite.net/lib/default.rb
+++ expoundite.net/lib/default.rb
@@ -3,3 +3,14 @@
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::Blogging
+
+def articles_essay
+  @items.select { |item| item[:extension] == 'page' && item.identifier.match(%r{/docs/}).nil? }.each
+end
+
+def articles_essay_sorted
+  articles_essay.sort_by do |a|
+    attribute_to_time(a[:mtime])
+  end.reverse
+end
