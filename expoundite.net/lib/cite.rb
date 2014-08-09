39db1d88ca20e5670efddb39b3bb4a1ec5ef4918
Haoyang-docker <github@expoundite.net>
2014-08-09 23:02:49 +0000

    cite filter prototype

--- expoundite.net/lib/cite.rb
+++ expoundite.net/lib/cite.rb
@@ -1,23 +1,27 @@
 module Nanoc::Filters
   class Cite < Nanoc::Filter
+    identifier :cite
     require "citeproc"
-    require "citeproc-ruby"
-    require "csl-styles"
+    # require "citeproc-ruby"
+    require "csl/styles"
 
     # setup, specify style and bib files
-    def run(content)
+    def run(content, params={})
       style = @item[:csl] || 'harvard'
       if @item[:bib].nil?
         return content
       else
-        bib = './bib/' + @item[:bib]
+        bib = 'content/bib/' + @item[:bib]
       end
 
       cp = CiteProc::Processor.new style: style, format: 'html'
       cp.import BibTeX.open(bib).to_citeproc # require bibtex-ruby
       
-      keys = content.scan(%r{\[@[A-Za-z0-9:-_].*?\]}) {|w| print "<<#{w}>>"} #test
-      content += keys
+      keys_p = content.scan(%r{\[@[A-Za-z0-9:-_].*?\]}).map {|key| key.gsub(/[@\[\]]/, '')}
+      keys_t = content.scan(%r{\b@[A-Za-z0-9:-_].*?}).map {|key| key.gsub(/@/, '')}
+      keys = (keys_p + keys_t).uniq
+      content += cp.render(:bibliography).to_s
+      content
     end
   end
 end
