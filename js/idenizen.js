53411ed7e93d1a570af8c38e26754295b57830cc
Haoyang Xu <github@expoundite.net>
2015-10-08 09:14:49 +0800

    page title display correct w/o banner pic

--- js/idenizen.js
+++ js/idenizen.js
@@ -7,3 +7,17 @@ $('document').ready(function() {
         $('span.tapestry').hide();
     });
 });
+
+$('document').ready(function() {
+    $('figure:first').hide(0, function() {
+	var imagePath = $('figure:first').children().attr('src');
+	$('div.content > h1').css(
+	    { 'background-image': 'url(' + imagePath +')',
+	      'background-position': '0% 0%',
+	      'height': '400px',
+	      'background-repeat': 'no-repeat',
+	      'background-clip': 'content-box' });
+	$('div.content > h1 > span').css(
+	    { 'top': '362px' });
+    });
+});
