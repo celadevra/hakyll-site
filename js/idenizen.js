26d280f942892557f1025e2b4d6af8f2884c7a39
Haoyang Xu <github@expoundite.net>
2015-10-08 09:39:36 +0800

    Make banner zoom with the page

--- js/idenizen.js
+++ js/idenizen.js
@@ -14,6 +14,7 @@ $('document').ready(function() {
 	$('div.content > h1').css(
 	    { 'background-image': 'url(' + imagePath +')',
 	      'background-position': '0% 0%',
+	      'background-size': 'cover',
 	      'height': '400px',
 	      'background-repeat': 'no-repeat',
 	      'background-clip': 'content-box' });
