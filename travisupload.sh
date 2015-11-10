c9c986fcfcb202ea19821f06b2e55a11018bff8f
Haoyang Xu <github@expoundite.net>
2015-11-10 19:55:21 +0800

    don't delete old files

--- travisupload.sh
+++ travisupload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-s3cmd sync _site/ s3://expoundite.net --config=.s3cfg --guess-mime-type --mime-type 'text/html' --recursive --delete-removed --acl-public --verbose
+s3cmd sync _site/ s3://expoundite.net --config=.s3cfg --guess-mime-type --mime-type 'text/html' --recursive --acl-public --verbose
