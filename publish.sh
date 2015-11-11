cfa1b636b0ee04aa4bac42a42fe28943e5a125b4
Haoyang Xu <github@expoundite.net>
2015-11-11 16:03:59 +0800

    exclude files without dots in 2nd upload

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 BUCKET=expoundite.net
 DIR=_site/
 aws s3 sync $DIR s3://$BUCKET/ --content-type "text/html" --exclude "*.js" --exclude "*.css" --exclude "*.jpg" --exclude "*.png"
-aws s3 sync $DIR s3://$BUCKET/ --include "*.js" --include "*.css" --include "*.jpg" --include "*.png" --exclude "[a-z]$"
+aws s3 sync $DIR s3://$BUCKET/ --include "*.js" --include "*.css" --include "*.jpg" --include "*.png" --exclude "[A-Za-z0-9-]*$"
