df0b9892abffc4eba80775aaf5e2c2564d551231
Haoyang Xu <github@expoundite.net>
2015-11-11 15:59:03 +0800

    upload twice with and w/o mime guessing

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 BUCKET=expoundite.net
 DIR=_site/
-aws s3 sync $DIR s3://$BUCKET/ --content-type "text/html"
+aws s3 sync $DIR s3://$BUCKET/ --content-type "text/html" --exclude "*.js" --exclude "*.css" --exclude "*.jpg" --exclude "*.png"
+aws s3 sync $DIR s3://$BUCKET/ --include "*.js" --include "*.css" --include "*.jpg" --include "*.png" --exclude "*"
