99fa08bc37b28108bfe73070d4f615fcfd795062
Haoyang Xu <github@expoundite.net>
2015-11-11 10:27:35 +0800

    fix build error

--- publish.sh
+++ publish.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-DEFAULT="expoundite"
-PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=expoundite.net
 DIR=_site/
-aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+aws  s3  sync $DIR s3://$BUCKET/
