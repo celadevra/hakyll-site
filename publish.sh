98ab243059dff129cb04f6df55f9acd4697ef900
Haoyang Xu <github@expoundite.net>
2015-11-11 16:02:33 +0800

    upload excluding files without dots

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 BUCKET=expoundite.net
 DIR=_site/
 aws s3 sync $DIR s3://$BUCKET/ --content-type "text/html" --exclude "*.js" --exclude "*.css" --exclude "*.jpg" --exclude "*.png"
-aws s3 sync $DIR s3://$BUCKET/ --include "*.js" --include "*.css" --include "*.jpg" --include "*.png" --exclude "*"
+aws s3 sync $DIR s3://$BUCKET/ --include "*.js" --include "*.css" --include "*.jpg" --include "*.png" --exclude "[a-z]$"
