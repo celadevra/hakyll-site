fc875550f59f0a2f1a20ecaceeb5a1719c34d751
Haoyang Xu <github@expoundite.net>
2015-11-13 09:46:59 +0800

    fix xml mime type

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 BUCKET=expoundite.net
 DIR=_site/
-aws s3 sync $DIR s3://$BUCKET/ --content-type "text/html" --exclude "*.js" --exclude "*.css" --exclude "*.jpg" --exclude "*.png" --exclude "*.ico"
-aws s3 sync $DIR s3://$BUCKET/ --include "*.js" --include "*.css" --include "*.jpg" --include "*.png" --include "*.ico" --exclude "[A-Za-z0-9-]*$"
+aws s3 sync $DIR s3://$BUCKET/ --content-type "text/html" --exclude "*.js" --exclude "*.css" --exclude "*.jpg" --exclude "*.png" --exclude "*.ico" --exclude "*.xml"
+aws s3 sync $DIR s3://$BUCKET/ --include "*.js" --include "*.css" --include "*.jpg" --include "*.png" --include "*.ico" --include "*.xml" --exclude "[A-Za-z0-9-]*$"
